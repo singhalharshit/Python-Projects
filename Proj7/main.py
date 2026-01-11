bd007b4bfba70d7b87ddcb6af0f05dea5893810c
Harshit Singhal <singhalharshit2910@gmail.com>
2026-01-11 08:30:28 -0500

    auth proj

---
+++ Proj7/main.py
@@ -0,0 +1,9 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+app=FastAPI()
+
+@app.get("/test")
+async def test():
+    return {"hello":"world"}
