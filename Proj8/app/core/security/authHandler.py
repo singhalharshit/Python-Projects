b0f44b1f34a72644d2ad97a4ecaa1297c98ec401
Harshit Singhal <singhalharshit2910@gmail.com>
2026-01-22 23:06:17 +0530

    furhter in proj

---
+++ Proj8/app/core/security/authHandler.py
@@ -0,0 +1,16 @@
+from jose import JWTError,jwt
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+SECRET_KEY="SOME_RANDOM_ACCESS_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=30
+
+
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class AuthHandler(object):
+    
+    @staticmethod
+    def sign_jwt(user_id:int):
+        
