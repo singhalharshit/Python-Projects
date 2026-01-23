7ba2390e9d80a74325776803e061d9caa62d0a1c
Harshit Singhal <singhalharshit2910@gmail.com>
2026-01-23 12:35:42 +0530

    done with proj

--- Proj7/main.py
+++ Proj7/main.py
@@ -17,7 +17,7 @@ fake_db =  {
         "username":"user_1",
         "full_name":"User One",
         "email": "user_one@user.com",
-        "hashed_password":"",
+        "hashed_password":"$2b$12$w8wWHPQ4SoV8S7RjlCzXfuUSZcr1BWkmNECNDD..iWTQ9DPlvkL1y",
         "disabled":"False"            
     }
 }
@@ -82,4 +82,42 @@ def create_access_token(data:dict,expires_delta:timedelta or None = None):
 
 
 async def get_current_user(token:str = Depends(oauth_2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Could not validate credentials",headers={"WWW-Authenticate":"Bearer"})
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username:str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credential_exception
     
+    user = get_user(fake_db,username=token_data.username)
+    if user is None:
+        raise credential_exception
+
+    return user
+
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400,detail="Inactive User")
+    return current_user
+
+@app.post("/token",response_model=Token)
+async def login_for_access_token(form_data:OAuth2PasswordRequestForm = Depends()):
+    user= authenticate_user(fake_db,form_data.username,form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Incorrect Username or Password",headers={"WWW-Authenticate":"Bearer"})
+    access_token_expires  = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data = {"sub":user.username},expires_delta=access_token_expires)
+    return {"access_token":access_token,"token_type":"bearer"}
+
+@app.get("/user/me",response_model=User)
+async def read_users(current_user:User=Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get("/user/items")
+async def read_users_items(current_user:User=Depends(get_current_active_user)):
+    return [{"item_id":1,"owner":current_user}]
