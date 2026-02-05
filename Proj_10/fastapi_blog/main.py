021f6a77a0f3a60197c6f374c5a66e2bcc7e5d20
Harshit Singhal <singhalharshit2910@gmail.com>
2026-02-05 13:46:22 +0530

    more in proj

--- Proj_10/fastapi_blog/main.py
+++ Proj_10/fastapi_blog/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI,Request
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/")
-def home():
-    return {"message":"Hello World!"}
+@app.get("/",include_in_schema=False)
+def home(request:Request):
+    return templates.TemplateResponse(request,"home.html")
